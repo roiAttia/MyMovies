382b78f03968bf14ef9ac2a29e7a519acdca5a70
roiAttia <roiattia.1@gmail.com>
2021-01-17 19:09:57 +0200

    added viewmodel di classes

---
+++ app/src/main/java/roiattia/com/mymovies/di/ViewModelsModule.kt
@@ -0,0 +1,12 @@
+package roiattia.com.mymovies.di
+
+import dagger.Module
+
+@Module
+abstract class ViewModelsModule {
+
+//    @Binds
+//    @IntoMap
+//    @ViewModelKey(TermsOfUseViewModel::class)
+//    abstract fun bindTermsOfUseViewModel(viewModel: TermsOfUseViewModel): ViewModel
+}
