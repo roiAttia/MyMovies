e50c995a4dc941174135a9c456cb79984a3fbb60
roiAttia <roiattia.1@gmail.com>
2021-01-23 18:28:49 +0200

    added home fragment

--- app/src/main/java/roiattia/com/mymovies/ui/home/HomeFragment.kt
+++ app/src/main/java/roiattia/com/mymovies/ui/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package roiattia.com.mymovies.ui.movies_list
+package roiattia.com.mymovies.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import roiattia.com.mymovies.R
 
-class MoviesListFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
