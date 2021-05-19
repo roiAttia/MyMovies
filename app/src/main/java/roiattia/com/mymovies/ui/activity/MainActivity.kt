bdd6cb4d3151c0b7a8f1b9087035b40d18832f2c
roiAttia <roiattia.1@gmail.com>
2021-05-19 11:35:29 +0300

    renamed movies list fragment

--- app/src/main/java/roiattia/com/mymovies/ui/activity/MainActivity.kt
+++ app/src/main/java/roiattia/com/mymovies/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package roiattia.com.mymovies.ui
+package roiattia.com.mymovies.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/roiattia/com/mymovies/ui/movies_list/MoviesListFragment.kt
+++ app/src/main/java/roiattia/com/mymovies/ui/movies_list/MoviesListFragment.kt
@@ -1,4 +1,4 @@
-package roiattia.com.mymovies.ui.home
+package roiattia.com.mymovies.ui.movies_list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import roiattia.com.mymovies.R
 
-class HomeFragment : Fragment() {
+class MoviesListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_movies_list, container, false)
     }
 }
